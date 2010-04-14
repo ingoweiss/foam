1e6a998c54cfc2b9b2636f90ff9a30a25f9d7bc0
Ingo Weiss <ingo@ingoweiss.com>
2010-04-14 14:45:33 +0200

    Fixed setting of 'skip-route' option in scaffold generator invocations

--- README.txt
+++ README.txt
@@ -1,11 +1,26 @@
 Names:
 
-- applicator
-- apptitude
-- defined_resources
-- RESTiny
-- resource_layout
-- relay (rails generate resource_layout --generator=ingoweiss:scaffold)
-- resolution
-- apply
-- appdef
+applicator
+apptitude
+defined_resources
+restiny
+resource_layout
+relay (rails generate resource_layout --generator=ingoweiss:scaffold)
+resolution
+apply
+appdef
+foam
+domsc (domain scaffold generator)
+dosca
+domain_scaffold_generator
+dome
+domato
+ddgen (domain-driven generator)
+domo
+crick (after Watson/Crick, the discoverers of DNS)      
+cricket
+appjet
+jumpstart
+daffodil
+scaffodil
+blossom
--- lib/generators/resource_layout/resource_layout_generator.rb
+++ lib/generators/resource_layout/resource_layout_generator.rb
@@ -23,9 +23,8 @@ class ResourceLayoutGenerator < Rails::Generators::Base
   def run_generators
     ResourceLayout.load(destination_root)
     generator_class = Rails::Generators.find_by_namespace(options[:generator].to_s)
-    ResourceLayout.resources.each do |resource|
-      resource_name, resource_attributes, resource_options = resource
-      generator_class.new([resource_name, resource_attributes_array(resource_attributes)], resource_options).invoke
+    ResourceLayout.resources.each do |(resource_name, resource_attributes, resource_options)|
+      generator_class.new([resource_name, resource_attributes_array(resource_attributes)], resource_options.merge(:skip_route => true)).invoke
       sleep 1.1 # Hack to prevent 'Multiple migrations have the version number' errors
     end
     route ResourceLayout.routes_definition
